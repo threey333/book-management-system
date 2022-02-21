1fca5401a912e7f1a28b3df21e5f747d522bce43
threey33 <229498183@qq.com>
2022-02-21 18:10:39 +0800

    将请求方法挂载在vue原型上

--- book-management-system-fe/src/main.js
+++ book-management-system-fe/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { service } from './service'
 
-createApp(App).use(store).use(router).mount('#app')
+// 导入scss入口文件
+import '@/assets/scss/index.scss'
+
+const app = createApp(App)
+
+/* 在app的原型上挂载service */
+app.config.globalProperties.$service = service
+
+app.use(store).use(router).mount('#app')
