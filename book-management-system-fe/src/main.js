37016b465f6a6f8a0ee9a14e3e91b04482fd4299
threey33 <229498183@qq.com>
2022-02-24 19:57:13 +0800

    处理ant组件的按需导入

--- book-management-system-fe/src/main.js
+++ book-management-system-fe/src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { service } from './service'
+import { Tabs } from 'ant-design-vue'
+import AntUI from '@/plugins/ant-design-ui'
 
 // 导入scss入口文件
 import '@/assets/scss/index.scss'
 
 const app = createApp(App)
+app.use(AntUI).use(Tabs)
 
 /* 在app的原型上挂载service */
 app.config.globalProperties.$service = service
---
+++ book-management-system-fe/src/plugins/ant-design-ui/index.js
@@ -0,0 +1,17 @@
+/**
+ * 说明: 由于ant design部分组件下还还还有其它组件，
+ * 对于这些组件我们是不能使用app.component()进行注册的，而是使用app.use()形式。
+*/
+
+import { Button, Input } from 'ant-design-vue'
+
+const components = [Button, Input]
+const AntUI = {
+  install: (app, options) => {
+    components.forEach(component => {
+      app.component(component.name, component)
+    })
+  }
+}
+
+export default AntUI
