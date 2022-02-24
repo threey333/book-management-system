ca489138648f04630d74b74e260ed30d23c7af83
threey33 <229498183@qq.com>
2022-02-24 19:53:31 +0800

    配置ant-design组件按需加载

--- book-management-system-fe/vue.config.js
+++ book-management-system-fe/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   assetsDir: 'static', // 放置静态资源的目录
   css: {
+    // requireModuleExtension: true,
     loaderOptions: {
       sass: {
         // sass-loader版本9以上使用的是additionalData
@@ -8,7 +9,15 @@ module.exports = {
           @import "@/assets/scss/variable.scss";
           @import "@/assets/scss/mixin.scss";
         `
+      },
+      less: {
+        lessOptions: {
+          // strictMath: true,
+          // noIeCompat: true,
+          javascriptEnabled: true
+        }
       }
+
     }
   }
 
