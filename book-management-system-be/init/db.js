ea7be4c8cac109dc77a1186d1acae9e342850a34
threey33 <229498183@qq.com>
2022-04-10 23:03:49 +0800

    完成角色集合初始化和用户初始化

--- book-management-system-be/init/db.js
+++ book-management-system-be/init/db.js
@@ -6,11 +6,22 @@ const { defaultCharacters } = character
 
 // 获取character集合
 const Character = mongoose.model('Character')
+const User = mongoose.model('User')
 
 connect().then(async () => {
-  console.log('开始初始化 角色 集合')
+  console.log('开始初始化 角色集合')
 
-  await Character.insertMany(defaultCharacters)
+  const characterList = await Character.insertMany(defaultCharacters)
 
-  console.log('角色集合初始化完成')
+  console.log('角色集合 初始化完成')
+
+  const user = new User({
+    account: 'admin',
+    password: '123456',
+    character: characterList.find(item => (item.name === 'member'))._id
+  })
+  console.log('正在初始化用户:', user)
+  await user.save()
+
+  console.log('用户初始化完成')
 })
